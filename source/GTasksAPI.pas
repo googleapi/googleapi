c58fafec9c8b8431d4ee20781f3640737fabb197
Vlad55 <vlad383@mail.ru>
2011-06-11 05:59:12 +0700

    Added: Google OAuth module (GoogleOAuth.pas) Google Tasks (GTasksAPI.pas) - laboratory api, base class
    Signed-off-by: Vlad55 <vlad383@mail.ru>

---
+++ source/GTasksAPI.pas
@@ -0,0 +1,343 @@
+﻿unit GTasksAPI;
+
+interface
+
+uses Classes, SysUtils, httpsend, GoogleOAuth, synacode, ssl_openssl,Dialogs;
+
+const
+  /// <summary>Версия API</summary>
+  APIVersion = '1';
+  /// <summary>Точка доступа к API для чтения и записи данных</summary>
+  APIScope = 'https://www.googleapis.com/auth/tasks';
+  /// <summary>Точка доступа к API только для чтения данных</summary>
+  APIScopeReadOnly = 'https://www.googleapis.com/auth/tasks.readonly';
+  /// <summary>шаблон составления URL для обращения к ресурсам API</summary>
+  URI = 'https://www.googleapis.com/tasks/v%s/%s/%s/%s%s';
+  /// <summary>Шаблон авторизации по протоколу OAuth 2.0</summary>
+ // AuthHeader = 'Authorization: OAuth %s';
+  /// <summary>Список с заданиями по умолчанию</summary>
+  DefaultList = '@default';
+  /// <summary>Пользователь по умолчанию</summary>
+  DefaultUser = '@me';
+
+type
+  {$REGION 'Описание класса'}
+  /// <summary>
+  /// Базовый класс для отправки запросов к API и получения ответов сервера.
+  ///  Все результаты выполнения функций передаются в виде строки, содержащей
+  ///  JSON-объекты, определенные в официальной документации:
+  ///  <exref target= "http://code.google.com/intl/ru-RU/apis/tasks/v1/reference.html">
+  ///  http://code.google.com/intl/ru-RU/apis/tasks/v1/reference.html
+  /// </summary>
+  {$ENDREGION}
+  TGTaskAPI = class
+  private
+    FOAuthClient: TOAuth;
+    function GetVersion: string;
+    procedure SetOAuthClient(const Value: TOAuth);
+  public
+    constructor Create;
+    destructor Destroy;override;
+    {$REGION 'Описание метода Lists.List'}
+    /// <summary>Возвращает все списки заданий для пользователя.
+    /// Набор свойств каждого спска заданий описан в официальной документации,
+    /// находящейся по адресу
+    /// <exref target= "http://code.google.com/intl/ru-RU/apis/tasks/v1/reference.html#resource_tasklists">
+    /// http://code.google.com/intl/ru-RU/apis/tasks/v1/reference.html#resource_tasklists
+    ///</summary>
+    /// <param name="maxResults">
+    ///  Максимальное количество элементов, возвращаемых в результате
+    /// </param>
+    /// <param name="pageToken">
+    ///  Токен страницы, которую необходимо вернуть в результате
+    /// </param>
+    /// <returns>
+    ///  <code>string</code><para>
+    ///   Возвращает JSON-объект, содержащий коллекцию списков заданий пользователя.
+    ///   Пример: <exref target= "http://code.google.com/intl/ru-RU/apis/tasks/v1/using.html#retrieving_task_lists">
+    /// в официальной документации
+    /// </returns>
+    {$ENDREGION}
+    function ListsList(maxResults: string = '';
+      pageToken: string = ''): string;
+    {$REGION 'Описание метода Lists.Get'}
+    /// <summary>Возвращает данные по одному списку заданий пользователя
+    /// Набор свойств каждого спска заданий описан в официальной документации,
+    /// находящейся по адресу
+    /// <exref target= "http://code.google.com/intl/ru-RU/apis/tasks/v1/reference.html#resource_tasklists"/>
+    /// http://code.google.com/intl/ru-RU/apis/tasks/v1/reference.html#resource_tasklists
+    ///</summary>
+    /// <param name="ListID">
+    /// Идентификатор списка
+    /// </param>
+    /// <returns>
+    /// <code>string</code><para>Возвращает JSON-объект, содержащий свойства списка<para>
+    ///  Пример: <exref target= "<exref target= "http://code.google.com/intl/ru-RU/apis/tasks/v1/reference.html#resource_tasklists"/>
+    ///  в официальной документации
+    /// </returns>
+    {$ENDREGION}
+    function ListsGet(const ListID: string): string;
+    {$REGION 'Описание метода List.Insert'}
+    /// <summary>Добавляет новый список заданий к аккаунту пользователя
+    ///  Список должен формироваться в JSON-формате и содержать одно или несколько свойств,
+    ///  определенных в официальной документации, расположенной по адресу:
+    ///  <exref target= "http://code.google.com/intl/ru-RU/apis/tasks/v1/reference.html#resource_tasklists"/>
+    ///  http://code.google.com/intl/ru-RU/apis/tasks/v1/reference.html#resource_tasklists
+    ///</summary>
+    /// <param name="JSONStream">
+    ///  Поток, содержащий JSON-объект списка заданий
+    /// </param>
+    /// <returns>
+    /// <code>string</code><para>Возвращает JSON-объект, содержащий свойства созданного списка<para>
+    ///  Пример: <exref target= "<exref target= "http://code.google.com/intl/ru-RU/apis/tasks/v1/using.html#creating_task_list"/>
+    ///  в официальной документации
+    /// </returns>
+    {$ENDREGION}
+    function ListsInsert(JSONStream: TStringStream):string;
+    {$REGION 'Описание метода Tasks.List'}
+    /// <summary>Возвращает набор всех заданий из определенного списка.
+    ///  Набор свойств для каждого задания определен в официальной документации,
+    ///  расположенной по адресу:
+    ///  <exref target= "http://code.google.com/intl/ru-RU/apis/tasks/v1/reference.html#resource_tasks"/>
+    ///  http://code.google.com/intl/ru-RU/apis/tasks/v1/reference.html#resource_tasks
+    ///</summary>
+    /// <param name="ListID">
+    /// Идентификатор списка
+    /// </param>
+    /// <returns>
+    ///   <code>string</code><para>
+    ///  Возвращает JSON-объект, содержащий коллекцию заданий из списка пользователя
+    ///  Пример: <exref target= "http://code.google.com/intl/ru-RU/apis/tasks/v1/using.html#retrieving_tasks"/>
+    ///  в официальной документации
+    /// </returns>
+    {$ENDREGION}
+    function TasksList(const ListID: string):string;overload;
+    {$REGION 'Описание метода Tasks.List'}
+    /// <summary>Возвращает набор всех заданий из определенного списка.
+    ///  Набор свойств для каждого задания определен в официальной документации,
+    ///  расположенной по адресу:
+    ///  <exref target= "http://code.google.com/intl/ru-RU/apis/tasks/v1/reference.html#resource_tasks"/>
+    ///  http://code.google.com/intl/ru-RU/apis/tasks/v1/reference.html#resource_tasks
+    ///</summary>
+    {$ENDREGION}
+    function TasksList(const ListID: string; Params:TStrings):string;overload;
+    {$REGION 'Описание метода Tasks.Get'}
+    /// <summary>Возвращает набор свойств определенного задания из списка пользователя
+    ///  Набор свойств для каждого задания определен в официальной документации,
+    ///  расположенной по адресу:
+    ///  <exref target= "http://code.google.com/intl/ru-RU/apis/tasks/v1/reference.html#resource_tasks"/>
+    ///  http://code.google.com/intl/ru-RU/apis/tasks/v1/reference.html#resource_tasks
+    ///</summary>
+    /// <param name="ListID">
+    /// Идентификатор списка
+    /// </param>
+    {$ENDREGION}
+    function TasksGet(const ListID: string; TaskID:string):string;overload;
+    function TasksGet(const TaskID: string):string;overload;
+    {$REGION 'Описание метода Tasks.Insert'}
+    /// <summary>Добавляет новое задание к списку пользователя
+    /// Задание должно формироваться в JSON-формате и содержать одно или несколько свойств,
+    /// определенных в официальной документации, расположенной по адресу:
+    /// <exref target= "http://code.google.com/intl/ru-RU/apis/tasks/v1/reference.html#resource_tasks"/>
+    /// http://code.google.com/intl/ru-RU/apis/tasks/v1/reference.html#resource_tasks
+    ///</summary>
+    /// <param name="ListID">
+    /// Идентификатор списка
+    /// </param>
+    /// <param name="Parent">
+    ///
+    /// </param>
+    /// <param name="Previous">
+    ///
+    /// </param>
+    /// <param name="JSONStream">
+    ///
+    /// </param>
+    {$ENDREGION}
+    function TasksInsert(const ListID, Parent, Previous: string; JSONStream: TStringStream):string; overload;
+    function TasksInsert(const ListID: string; JSONStream: TStringStream):string; overload;
+    function TasksInsert(const JSONStream: TStringStream):string; overload;
+   {$REGION 'Описание метода Tasks.Insert'}
+    /// <summary>
+    ///
+    ///</summary>
+    /// <param name="ListID">
+    /// Идентификатор списка
+    /// </param>
+    /// <param name="Parent">
+    ///
+    /// </param>
+    /// <param name="Previous">
+    ///
+    /// </param>
+    /// <param name="JSONStream">
+    ///
+    /// </param>
+    {$ENDREGION}    function TasksMove(const ListID, TaskID, parentTaskID, previousTaskID:string):string;overload;
+    function TasksMove(const TaskID, parentTaskID, previousTaskID:string):string;overload;
+
+    function TasksUpdate(const ListID,TaskID:string; JSONStream: TStringStream):string;overload;
+    function TasksUpdate(const TaskID:string; JSONStream: TStringStream):string;overload;
+
+    function TasksDelete(const ListID,TaskID:string):boolean;overload;
+    function TasksDelete(const TaskID:string):boolean;overload;
+
+    {$REGION 'Описание свойства Version'}
+    /// <summary></summary>
+    {$ENDREGION}
+    property Version: string read GetVersion;
+
+    property OAuthClient: TOAuth read FOAuthClient write SetOAuthClient;
+  end;
+
+implementation
+
+{ TGTaskAPI }
+
+constructor TGTaskAPI.Create;
+begin
+  inherited Create;
+  FOAuthClient:=TOAuth.Create(nil);
+end;
+
+destructor TGTaskAPI.Destroy;
+begin
+  FOAuthClient.Free;
+  inherited Destroy;
+end;
+
+function TGTaskAPI.GetVersion: string;
+begin
+  Result := APIVersion;
+end;
+
+function TGTaskAPI.ListsGet(const ListID: string): string;
+begin
+  Result := UTF8ToString(OAuthClient.GETCommand(Format(URI, [Version, 'users', DefaultUser,
+    'lists', '/' + ListID]), nil));
+end;
+
+function TGTaskAPI.ListsInsert(JSONStream: TStringStream): string;
+begin
+  Result:=UTF8ToString(OAuthClient.POSTCommand(Format(URI,[Version,'users',DefaultUser,'lists','']),nil,JSONStream))
+end;
+
+function TGTaskAPI.ListsList(maxResults, pageToken: string): string;
+var
+  Params: TStrings;
+  URL: string;
+begin
+  URL := Format(URI, [Version, 'users', DefaultUser, 'lists', '']);
+  Params := TStringList.Create;
+  try
+    if Length(Trim(maxResults)) > 0 then
+      Params.Add('maxResults=' + maxResults);
+    if Length(Trim(pageToken)) > 0 then
+      Params.Add('pageToken=' + pageToken);
+    Result := UTF8ToString(OAuthClient.GETCommand(URL, Params));
+  finally
+    Params.Free;
+  end;
+end;
+
+procedure TGTaskAPI.SetOAuthClient(const Value: TOAuth);
+begin
+  FOAuthClient := Value;
+end;
+
+function TGTaskAPI.TasksList(const ListID: string): string;
+begin
+  Result:=TasksList(ListID,nil)
+end;
+
+function TGTaskAPI.TasksGet(const ListID: string; TaskID: string): string;
+begin
+Result := UTF8ToString(OAuthClient.GETCommand(Format(URI, [Version, 'lists', ListID,
+    'tasks', '/'+TaskID]), nil));
+end;
+
+function TGTaskAPI.TasksDelete(const ListID, TaskID: string): boolean;
+begin
+  Result:=Length(OAuthClient.DELETECommand(Format(URI, [Version, 'lists', ListID,
+    'tasks', '/'+TaskID])))=0
+end;
+
+function TGTaskAPI.TasksDelete(const TaskID: string): boolean;
+begin
+  Result:=TasksDelete(DefaultList,TaskID);
+end;
+
+function TGTaskAPI.TasksGet(const TaskID: string): string;
+begin
+  Result:=TasksGet(DefaultList,TaskID);
+end;
+
+function TGTaskAPI.TasksInsert(const JSONStream: TStringStream): string;
+begin
+  Result:=TasksInsert(DefaultList,JSONStream)
+end;
+
+function TGTaskAPI.TasksInsert(const ListID, Parent, Previous: string;
+  JSONStream: TStringStream): string;
+var Params:TStrings;
+begin
+  Params:=TStringList.Create;
+  try
+    if Length(Trim(Parent))>0 then
+      Params.Values['parent']:=Parent;
+    if Length(Trim(Previous))>0 then
+      Params.Values['previous']:=Previous;
+    Result:=UTF8ToString(OAuthClient.POSTCommand(Format(URI,[Version,'lists',ListId,'tasks','']),Params,JSONStream));
+  finally
+    Params.Free;
+  end;
+end;
+
+function TGTaskAPI.TasksInsert(const ListID: string;
+  JSONStream: TStringStream): string;
+begin
+  Result:=TasksInsert(ListID,'','',JSONStream);
+end;
+
+function TGTaskAPI.TasksList(const ListID: string; Params: TStrings): string;
+begin
+  Result := UTF8ToString(OAuthClient.GETCommand(Format(URI, [Version, 'lists', ListID,
+    'tasks', '']), Params));
+end;
+
+function TGTaskAPI.TasksMove(const TaskID, parentTaskID,
+  previousTaskID: string): string;
+begin
+  Result:=TasksMove(DefaultList,TaskID,parentTaskID,previousTaskID)
+end;
+
+function TGTaskAPI.TasksUpdate(const TaskID: string;
+  JSONStream: TStringStream): string;
+begin
+  Result:=TasksUpdate(DefaultList,TaskID,JSONStream);
+end;
+
+function TGTaskAPI.TasksUpdate(const ListID, TaskID: string;JSONStream: TStringStream): string;
+begin
+  Result := UTF8ToString(OAuthClient.PUTCommand(Format(URI, [Version, 'lists', ListID,
+    'tasks', '/'+TaskID]),JSONStream));
+end;
+
+function TGTaskAPI.TasksMove(const ListID, TaskID, parentTaskID,
+  previousTaskID: string): string;
+var Params: TStrings;
+begin
+Params:=TStringList.Create;
+try
+  if Length(Trim(parentTaskID))>0 then
+    Params.Values['parent']:=parentTaskID;
+  if Length(Trim(previousTaskID))>0 then
+    Params.Values['previous']:=previousTaskID;
+  Result:=UTF8ToString(OAuthClient.POSTCommand(Format(URI,[Version,'lists',ListID,'tasks',TaskID,'/move']),Params,nil));
+finally
+  Params.Free;
+end;
+
+end;
+
+end.
---
+++ source/GoogleOAuth.pas
@@ -0,0 +1,250 @@
+﻿unit GoogleOAuth;
+
+interface
+
+uses SysUtils, Classes, httpsend, ssl_Openssl,character,synacode;
+
+resourcestring
+  rsRequestError = 'Ошибка выполнения запроса: %d - %s';
+
+const
+  redirect_uri='urn:ietf:wg:oauth:2.0:oob';
+  oauth_url = 'https://accounts.google.com/o/oauth2/auth?client_id=%s&redirect_uri=%s&scope=%s&response_type=code';
+  tokenurl='https://accounts.google.com/o/oauth2/token';
+  tokenparams = 'client_id=%s&client_secret=%s&code=%s&redirect_uri=%s&grant_type=authorization_code';
+  crefreshtoken = 'client_id=%s&client_secret=%s&refresh_token=%s&grant_type=refresh_token';
+  AuthHeader = 'Authorization: OAuth %s';
+
+  DefaultMime = 'application/json; charset=UTF-8';
+
+  StripChars : set of char = ['"',':',','];
+
+type
+  TOAuth = class(TComponent)
+  private
+    FClientID: string;//id клиента
+    FClientSecret: string;//секретный ключ клиента
+    FScope   : string;//точка доступа
+    FResponseCode: string;
+    //Токен
+    FAccess_token: string;
+    FExpires_in: string;
+    FRefresh_token:string;
+    procedure SetClientID(const Value: string);
+    procedure SetResponseCode(const Value: string);
+    procedure SetScope(const Value: string);//код, который возвращает Google для доступа
+    function  ParamValue(ParamName,JSONString: string):string;
+    procedure SetClientSecret(Value: string);
+    function PrepareParams(Params: TStrings): string;
+  public
+    constructor Create(AOwner: TComponent);override;
+    destructor destroy; override;
+    function AccessURL: string; //собирает URL для получения ResponseCode
+    function GetAccessToken: string;
+    function RefreshToken: string;
+
+    function GETCommand(URL: string; Params: TStrings): RawBytestring;
+    function POSTCommand(URL:string; Params:TStrings; Body:TStream; Mime:string = DefaultMime):RawByteString;
+    function PUTCommand(URL:string; Body:TStream; Mime:string = DefaultMime):RawByteString;
+    function DELETECommand(URL:string):RawByteString;
+
+    //Параметры токена (сам токен, время действия, ключ для обновления
+    property Access_token: string read FAccess_token;
+    property Expires_in: string read FExpires_in;
+    property Refresh_token:string read FRefresh_token;
+    property ResponseCode: string read FResponseCode write SetResponseCode;
+  published
+    property ClientID: string read FClientID write SetClientID;
+    property Scope   : string read FScope write SetScope;
+    property ClientSecret: string read FClientSecret write SetClientSecret;
+end;
+
+implementation
+
+{ TOAuth }
+
+function TOAuth.AccessURL: string;
+begin
+  Result:=Format(oauth_url,[ClientID,redirect_uri,Scope]);
+end;
+
+constructor TOAuth.Create(AOwner: TComponent);
+begin
+  inherited Create(AOwner);
+end;
+
+function TOAuth.DELETECommand(URL: string): RawByteString;
+begin
+with THTTPSend.Create do
+  begin
+    Headers.Add(Format(AuthHeader, [Access_token]));
+    if HTTPMethod('DELETE', URL) then
+    begin
+      SetLength(Result, Document.Size);
+      Move(Document.Memory^, Pointer(Result)^, Document.Size);
+    end
+    else
+      raise Exception.CreateFmt(rsRequestError,[ResultCode,ResultString]);
+  end;
+end;
+
+destructor TOAuth.destroy;
+begin
+
+  inherited;
+end;
+
+function TOAuth.GetAccessToken: string;
+var Params: TStringStream;
+    Response:string;
+begin
+  Params:=TStringStream.Create(Format(tokenparams,[ClientID,ClientSecret,ResponseCode,redirect_uri]));
+  try
+    Response:=POSTCommand(tokenurl,nil,Params,'application/x-www-form-urlencoded');
+    FAccess_token:=ParamValue('access_token',Response);
+    FExpires_in:=ParamValue('expires_in',Response);
+    FRefresh_token:=ParamValue('refresh_token',Response);
+    Result:=Access_token;
+  finally
+    Params.Free;
+  end;
+end;
+
+function TOAuth.GETCommand(URL: string; Params: TStrings): RawBytestring;
+var
+  ParamString: string;
+begin
+  ParamString := PrepareParams(Params);
+  with THTTPSend.Create do
+  begin
+    Headers.Add(Format(AuthHeader, [Access_token]));
+    if HTTPMethod('GET', URL + ParamString) then
+    begin
+      SetLength(Result, Document.Size);
+      Move(Document.Memory^, Pointer(Result)^, Document.Size);
+    end
+    else
+    begin
+      raise Exception.CreateFmt(rsRequestError,[ResultCode,ResultString]);
+    end;
+  end;
+end;
+
+function TOAuth.ParamValue(ParamName, JSONString: string): string;
+var i,j:integer;
+begin
+  i:=pos(ParamName,JSONString);
+  if i>0 then
+    begin
+      for j:= i+Length(ParamName) to Length(JSONString)-1 do
+        if not (JSONString[j] in StripChars) then
+          Result:=Result+JSONString[j]
+        else
+          if JSONString[j]=',' then
+            break;
+    end
+  else
+    Result:='';
+end;
+
+function TOAuth.POSTCommand(URL: string; Params: TStrings;
+  Body: TStream; Mime:string): RawByteString;
+var ParamString: string;
+begin
+ParamString := PrepareParams(Params);
+  with THTTPSend.Create do
+  begin
+    MimeType:=Mime;
+    Headers.Add(Format(AuthHeader, [Access_token]));
+    if Body<>nil then
+      begin
+      Body.Position:=0;
+      Document.LoadFromStream(Body);
+      end;
+    if HTTPMethod('POST', URL + ParamString) then
+    begin
+      SetLength(Result, Document.Size);
+      Move(Document.Memory^, Pointer(Result)^, Document.Size);
+    end
+    else
+    begin
+      raise Exception.CreateFmt(rsRequestError,[ResultCode,ResultString]);
+    end;
+  end;
+end;
+
+function TOAuth.PrepareParams(Params: TStrings): string;
+var
+  S: string;
+begin
+  if Assigned(Params) then
+    if Params.Count > 0 then
+    begin
+      for S in Params do
+        Result := Result + EncodeURL(S) + '&';
+      Delete(Result, Length(Result), 1);
+      Result:='?'+Result;
+      Exit;
+    end;
+  Result := '';
+end;
+
+function TOAuth.PUTCommand(URL: string; Body: TStream; Mime:string): RawByteString;
+begin
+with THTTPSend.Create do
+  begin
+    MimeType:=Mime;
+    Headers.Add(Format(AuthHeader, [Access_token]));
+    if Body<>nil then
+      begin
+        Body.Position:=0;
+        Document.LoadFromStream(Body);
+      end;
+    if HTTPMethod('PUT', URL) then
+    begin
+      SetLength(Result, Document.Size);
+      Move(Document.Memory^, Pointer(Result)^, Document.Size);
+    end
+    else
+    begin
+      raise Exception.CreateFmt(rsRequestError,[ResultCode,ResultString]);
+    end;
+  end;
+end;
+
+function TOAuth.RefreshToken: string;
+var Params: TStringStream;
+    Response: string;
+begin
+  Params:=TStringStream.Create(Format(crefreshtoken,[ClientID,ClientSecret,Refresh_token]));
+  try
+    Response:=POSTCommand(tokenurl,nil,Params,'application/x-www-form-urlencoded');
+    FAccess_token:=ParamValue('access_token',Response);
+    FExpires_in:=ParamValue('expires_in',Response);
+    Result:=Access_token;
+  finally
+    Params.Free;
+  end;
+end;
+
+procedure TOAuth.SetClientID(const Value: string);
+begin
+  FClientID := Value;
+end;
+
+procedure TOAuth.SetClientSecret(Value: string);
+begin
+  FClientSecret:=EncodeURL(Value)
+end;
+
+procedure TOAuth.SetResponseCode(const Value: string);
+begin
+  FResponseCode := Value;
+end;
+
+procedure TOAuth.SetScope(const Value: string);
+begin
+  FScope := Value;
+end;
+
+end.
